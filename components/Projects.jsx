fbc68c92bad66749f6db50dde21c8dc401e247b1
Ruan Pablo <ruan_goio10@hotmail.com>
2022-05-31 18:35:36 -0300

    Projecs Component Container

---
+++ components/Projects.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/no-unescaped-entities */
+
+
+export function Projects() {
+    return (
+        <div className="w-full">
+            <div className="max-w-[1240px] mx-auto px-2 py-16">
+                <p className="text-xl tracking-widest uppercase text-[#1F6FEB]">Projects</p>
+                <h2 className="py-4">What I've Build</h2>
+
+                <div className="gird md:grid-cols-2 gap-8">
+
+                    <div className="
+                        relative flex items-center justify-center 
+                        h-auto w-full shadow-xl shadow-gray-400 
+                        rounded-xl 
+                        p-4 group hover:bg-gradient-to-r
+                      from-[#425675]
+                      to-[#1F6FEB]"
+                    >
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
