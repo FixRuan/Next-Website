2d903bc7d9db09941df3bdcb6cfd1403414f10b8
Ruan Pablo <ruan_goio10@hotmail.com>
2022-06-01 18:39:10 -0300

    Project Image group

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,4 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+import Link from "next/link";
+import DevlandiaImage from '../public/assets/projects/devlandia.png';
 
 
 export function Projects() {
@@ -11,14 +14,20 @@ export function Projects() {
                 <div className="gird md:grid-cols-2 gap-8">
 
                     <div className="
-                        relative flex items-center justify-center 
-                        h-auto w-full shadow-xl shadow-gray-400 
-                        rounded-xl 
-                        p-4 group hover:bg-gradient-to-r
-                      from-[#425675]
-                      to-[#1F6FEB]"
+                        relative flex items-center justify-center h-auto w-full 
+                        shadow-xl shadow-gray-400 rounded-xl p-4 group 
+                        hover:bg-gradient-to-r
+                        from-[#425675]
+                        to-[#1F6FEB]"
                     >
-
+                        <Image className="rounded-xl group-hover:opacity-10" src={DevlandiaImage} alt="/" />
+                        <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                            <h3>Devlândia</h3>
+                            <p>React Native</p>
+                            <Link href="/">
+                                <p>More Info</p>
+                            </Link>
+                        </div>
                     </div>
 
                 </div>
